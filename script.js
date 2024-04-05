0f9b311c9de79a0395f4dfc3d84f4aa7cf1aa35a
Prajapathi <yadavallisyam1@gmail.com>
2024-04-04 22:52:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// import Typewriter from "typewriter-effect/dist/core";
+
+// var typewriter = new Typewriter(".type", {});
+
+// typewriter.typeString("<strong>Happy Birthday Ushasree</strong>").start();
+// //   .pauseFor(300)
+// //   .deleteChars(10)
+// //   .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
+// //   .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+// //   .pauseFor(1000)
+// //   .start();
+var modal_button = document.getElementById('cta-button');
+var modal = document.getElementById('modal');
+
+// console.log(modal_button)
+
+modal_button.addEventListener('click', (e) => {
+    // modal.style.display = 'block';
+    console.log(e.target)
+})
